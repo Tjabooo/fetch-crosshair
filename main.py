43a885dacaf2ab0dcab4d54b79b59ffe454236e0
Tjabooo <39222357+Tjabooo@users.noreply.github.com>
2023-11-19 21:36:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import keyboard, threading, json
 from os import system
 
-# Define the hotkey
 keyboard.add_hotkey('ctrl+left windows+alt+p', lambda: on_hotkey())
 
-# Define the function that gets called when the hotkey is triggered
 def on_hotkey():
     print("Hotkey triggered!")
     t = threading.Thread(target=record_keystrokes)
     t.start()
 
-
 def record_keystrokes():
-    print("Recording...")
-    # Record keystrokes until Enter is pressed
     recorded = keyboard.record(until='enter')
     crosshair = ''.join([key.name for key in recorded if key.event_type == 'down' and key.name != 'enter'])
     try:
@@ -23,6 +18,4 @@ def record_keystrokes():
     except KeyError:
         pass
 
-
-# Start the keyboard listener
 keyboard.wait()
